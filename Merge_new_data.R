be25d579d5b1b8595d1da59e4f9624aec2cf3382
Fernando Rodriguez <63273349+fernandojru@users.noreply.github.com>
2020-04-09 21:18:45 -0600

    Add files via upload
    Updated with April 8th data

---
+++ Merge_new_data.R
@@ -0,0 +1,15 @@
+COVID_2<-read.csv("COVID19_7-Apr.csv")
+
+New_COVID<-read.csv("04-08-2020.csv")
+
+New_COVID <- New_COVID %>% select(-c("FIPS","Admin2","Last_Update","Active","Combined_Key"))
+
+New_COVID <- New_COVID %>% rename("Province.State"="Province_State",
+                                  "Country.Region"="Country_Region",
+                                  "Long"="Long_")
+
+New_COVID$Date<-"4/8/20"
+
+Update_COVID<-rbind(COVID_2,New_COVID)
+
+write.csv(Update_COVID,"COVID19_8-Apr.csv")
