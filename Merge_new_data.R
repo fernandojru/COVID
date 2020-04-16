479ab4e2526ecfee350807be7059f45693001188
Fernando Rodriguez <63273349+fernandojru@users.noreply.github.com>
2020-04-16 09:59:43 -0600

    Add files via upload

--- Merge_new_data.R
+++ Merge_new_data.R
@@ -1,8 +1,8 @@
-COVID_2<-read.csv("COVID19_13-Apr.csv")
+COVID_2<-read.csv("COVID19_14-Apr.csv")
 
 COVID_2<-COVID_2 %>% select(-c("X"))
 
-New_COVID<-read.csv("04-14-2020.csv")
+New_COVID<-read.csv("04-15-2020.csv")
 
 New_COVID <- New_COVID %>% select(-c("FIPS","Admin2","Last_Update","Active","Combined_Key"))
 
@@ -10,8 +10,8 @@ New_COVID <- New_COVID %>% rename("Province.State"="Province_State",
                                   "Country.Region"="Country_Region",
                                   "Long"="Long_")
 
-New_COVID$Date<-"4/14/20"
+New_COVID$Date<-"4/15/20"
 
 Update_COVID<-rbind(COVID_2,New_COVID)
 
-write.csv(Update_COVID,"COVID19_14-Apr.csv")
+write.csv(Update_COVID,"COVID19_15-Apr.csv")
